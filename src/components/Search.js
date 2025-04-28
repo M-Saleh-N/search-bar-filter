f46ebb75b859730a04a46731486a473603d3d5e6
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-28 10:03:05 +0300

    added names of cars in to a list and added a function

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./Search.css";
+
+const items = [
+    'Tesla Model S', 'Ford Mustang', 'Chevrolet Camaro', 'Porsche 911', 'Lamborghini Huracán',
+    'Toyota Supra', 'BMW M3', 'Mercedes-Benz C-Class', 'Audi R8', 'Nissan GT-R',
+    'Honda Civic Type R', 'Ferrari F8 Tributo', 'McLaren 720S', 'Subaru WRX', 'Dodge Charger',
+    'Jaguar F-Type', 'Mazda MX-5', 'Volkswagen Golf GTI', 'Aston Martin DB11', 'Kia Stinger',
+    'Hyundai Veloster', 'Bugatti Chiron'
+  ];
+
+function Search() {
+  const [filter, setFilter] = useState("");
+  const filteredItems = items.filter(item => item.toLowerCase().includes(filter.toLowerCase()));
+
+}
+
+export default Search;
