fd62d56a57ed6c4e89dac17c68e92591451b2782
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-28 10:04:57 +0300

    addded content

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,6 +13,17 @@ function Search() {
   const [filter, setFilter] = useState("");
   const filteredItems = items.filter(item => item.toLowerCase().includes(filter.toLowerCase()));
 
+  return (
+    <div className="search-cont">
+      <input type="text" placeholder="Search For a Car" value={filter} 
+        onChange={(e) => setFilter(e.target.value)} className="search-input" />
+      <ul className="result-li">
+        {filteredItems.length ? filteredItems.map((item, index) => (
+          <li key={index} className="result-item">{item}</li>
+        )) : <li className="no-results">No match found.</li>}
+      </ul>
+    </div>
+  );
 }
 
 export default Search;
